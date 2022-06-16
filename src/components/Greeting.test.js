abc1108c2d1691ea4aa6e951a52818b91d3683fc
dsampath <dsampath@wiley.com>
2022-06-16 12:16:21 +0530

    added a first simple test to validate a text is shown on the screen

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,21 @@
+//here were are trying to test whether we have hello world on the screen
+
+import Greeting from "./Greeting";
+import {render, screen} from "@testing-library/react";
+
+test('renders Hello World as a test', () => {
+  //Arrange
+  render(<Greeting />);
+
+  //Act
+    //... nothing
+
+  //Asert
+  const hellowWorldElement = screen.getByText('Hellow World!', {exact: true});
+  expect(hellowWorldElement).toBeInTheDocument();
+  // expect(hellowWorldElement).not.toBeInTheDocument();
+});
+
+//get functions-> return error if not found
+//query functions-> does not return error
+//find functions-> want a promise
