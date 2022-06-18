c74b0ecee944641535e40c80589a739d32a9f830
dsampath <dsampath@wiley.com>
2022-06-18 12:45:13 +0530

    writing unit tests when there is a state change

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -2,6 +2,7 @@
 
 import Greeting from "./Greeting";
 import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";//this is an object that helps us to trigger user events
 
 describe('Greeting component', () => {
     test('renders Hello World as a test', () => {
@@ -11,10 +12,49 @@ describe('Greeting component', () => {
         //Act
         //... nothing
 
-        //Asert
+        //Assert
         const hellowWorldElement = screen.getByText('Hellow World!', {exact: true});
         expect(hellowWorldElement).toBeInTheDocument();
         // expect(hellowWorldElement).not.toBeInTheDocument();
+    });
+
+    test('renders good to see you when button NOT clicked', () => {
+        //Arrange
+        render(<Greeting/>);
+
+        //Act: No Action needs to be done by user in this test case
+
+        //Assert
+        const goodToSeeYouTextElement = screen.getByText('Its good to see you', {exact: false});
+        expect(goodToSeeYouTextElement).toBeInTheDocument();
+    });
+
+    test('renders good to see you when button clicked', () => {
+        //Arrange
+        render(<Greeting/>);
+
+        //Act: now here we need to click the button as user do
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);//this simulate the button click
+
+        //Assert
+        const changedTextElement = screen.getByText('Changed');
+        expect(changedTextElement).toBeInTheDocument();
+    });
+
+    //below test is a negative scenario. it checks whether "good to see you" is there after the button click.
+    // ideally it should not be there. so that also a test case.
+    test('does not render "good to see you" if the button was clicked', () => {
+        //Arrange
+        render(<Greeting/>);
+
+        //Act: now here we need to click the button as user do
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);//this simulate the button click
+
+        //Assert
+        const outputElement = screen.queryByText('good to see you', {exact: false});
+        expect(outputElement).toBeNull();
     });
 })
 
